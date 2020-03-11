78460e2062d60557ecb244b464c6a42636f84296
Ty Bandachit <tbandachit@gmail.com>
2020-03-11 22:16:17 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,28 @@
-
-
+class Book
+  
+  attr_accessor :author, :page_count, :genre
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def page_count=(count)
+    @page_count = count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end 
