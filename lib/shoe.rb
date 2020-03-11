7946d49ed6b50256dd6a26c82a37f460f3f32348
Tisdale511 <tisdalefry@gmail.com>
2020-03-10 18:46:53 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,55 @@
 # Make your shoe class here!
+class Shoe
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand=(brand)
+        @brand = "Nike"
+    end
+
+    def brand
+        @brand
+    end
+
+    def color=(color)
+        @color = "red"
+    end
+
+    def color
+        @color
+    end
+
+    def size=(size)
+        @size = 9.5
+    end
+
+    def size
+        @size
+    end
+
+    def material=(material)
+        @material = "suede"
+    end
+
+    def material
+        @material
+    end
+
+    def condition=(condition)
+        @condition = 'tattered'
+    end
+
+    def condition
+        @condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
+shoe = Shoe.new("")
+shoe.cobble
